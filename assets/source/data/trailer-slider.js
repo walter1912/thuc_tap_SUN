9d183c446e1939a0b63ae58efa1254e8bcbd0189
walter1912 <thaiho2k@gmail.com>
2023-07-13 15:46:47 +0700

    done header and slider up-next

---
+++ assets/source/data/trailer-slider.js
@@ -0,0 +1,13 @@
+export const trailer_slider = [
+  {
+    name: "",
+    img: {
+      title: "5 Things to Watch on Netflix",
+      src: "https://m.media-amazon.com/images/M/MV5BMTYzOWVlNDUtMTdkYi00YTgzLWIyODctMzZhZGZmYWEyMGViXkEyXkFqcGdeQWplZmZscA@@._V1_QL40_QL75_UX500_CR0,0,500,281_.jpg",
+    },
+    poster: {
+        title:'5 New Netflix Movies and Shows to Watch for the Week of June 26',
+      src: "https://m.media-amazon.com/images/M/MV5BNDYwOWUyMDItZTlkZi00YzYxLThlMmMtMzM5Yzk2NjVkZDliXkEyXkFqcGdeQXVyNjMwMzc3MjE@._V1_QL75_UX140_CR0,0,140,207_.jpg",
+    },
+  },
+];
